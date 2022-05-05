45cc7b2172e2c3680743cb25983a5f2176fe64a7
JLXP <javier.xool@outlook.com>
2022-05-05 10:38:33 -0500

    first commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
         <header class="p-5 border-b bg-white shadow">
             <div class="container mx-auto flex justify-between items-center">
                 <h1 class="text-3xl font-black">
-                    DevStagram
+                    DevStagram New
                 </h1>
 
                 <nav class="flex gap-2 items-center">
